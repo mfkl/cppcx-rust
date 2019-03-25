35540f3edccea2ca638134595643e6b022af3e5e
Martin Finkel <martin@videolabs.io>
2019-03-25 18:20:56 +0100

    add basic cppcx app

---
+++ App2/MainPage.xaml.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace App2;
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	
+	HMODULE ret = LoadPackagedLibrary(L"hello_world.dll", 0);
+	FARPROC r = GetProcAddress(ret, "hello");
+	INT_PTR ptr = r();
+
+	int number = ptr;
+
+}
